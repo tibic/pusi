bbcccb1e43772c504794ce35f0c5b79b009caa26
tibic <1759698596@qq.com>
2016-04-16 10:36:40 +0800

    fix

---
+++ conn.php
@@ -0,0 +1,14 @@
+<?php
+
+$conn = @mysql_connect("localhost","root","");
+if (!$conn){
+    die("连接数据库失败：" . mysql_error());
+}
+mysql_select_db("psi", $conn);
+//字符转换，读库
+mysql_query("set character set 'utf-8'");
+//写库
+mysql_query("set names 'utf-8'");
+?>
+
+
